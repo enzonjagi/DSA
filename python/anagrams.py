7289f66c46e7bd2ba406f1a4f3b64b932af19589
enzonjagi <amosndungo@gmail.com>
2023-05-17 12:00:00 +0300

    Empty file - Finding out if two strings supplied are anagrams of each other

---
+++ python/anagrams.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+The purpose of this file is to check if two strings supplied are anagrams.
+Anagram is a string formed by rearranging another string.
+
+The idea is to check if all the letters in 'string_a',
+are all the strings in 'string_b'.
+
+Steps I'll be using:
+    1. Receive input from the command line.
+    2. 
+
+
+TODO Add the complexity analysis of this file here:
+"""
