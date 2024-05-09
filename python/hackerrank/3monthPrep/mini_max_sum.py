b0b3a8b083051c1fee720e1d92e58cb4cce46fc5
Nj <92800887+omarndungo@users.noreply.github.com>
2024-05-09 11:45:37 +0300

    Hacckerrank 3 month prep: miniMaxSum challenge.

---
+++ python/hackerrank/3monthPrep/mini_max_sum.py
@@ -0,0 +1,69 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+"""
+Goal: Minimum sum and maximum sum
+
+Functions:
+    1. minSum(arr): calculates the minimum sum
+    2. maxSum(arr): calculates the maximum sum
+    3. miniMaxSum(arr): output the minimum and maximum 
+    sums of the items in the array.
+"""
+def sumArr(arr):
+    # returns a sorted arr
+    sumArr = 0
+    
+    for a in arr:
+        sumArr += a
+        
+    return sumArr
+    
+def minSum(arr):
+    # returns the sum of the first four items.
+    length = len(arr)
+    minArr = arr[0:length-1]
+    
+    minSum = sumArr(minArr)
+    
+    return minSum
+    
+def maxSum(arr):
+    # returns the sum of the first four items.
+    length = len(arr)
+    maxArr = arr[1:length]
+    
+    maxSum = sumArr(maxArr)
+    
+    return maxSum
+    
+
+def miniMaxSum(arr):
+    # Write your code here
+    length = len(arr)
+    
+    if length < 5:
+        if length <= 0:
+            return 0
+        return sum(arr[0:length-1])
+         
+    miniSum = minSum(arr)
+    maxiSum = maxSum(arr)
+    
+    print(f"{miniSum} {maxiSum}")
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
