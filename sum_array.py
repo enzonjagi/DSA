c9adec8896222b618754b08d3629fc19d7753643
Umar Njagi Ndungo <92800887+umarnjagitech@users.noreply.github.com>
2025-04-16 16:34:40 +0000

    Sum of elements in an array

--- sum_array.py
+++ sum_array.py
@@ -14,19 +14,16 @@ Problem:
 
 To Think about
     Check for an array - input should be an array
-    if not - inform or request for array
+    if not - inform or request for elements of an array
 
-    if array:
-        check for empty array - return 0
-    else:
-        TODO: check if array elements are integers?
+    TODO: check if array elements are integers?
 
-        we could call the sum function with the array
+    we could call the sum function with the array
 
-        or
+    or
 
-        we could have a total variable of value 0,
-        then loop through array adding each element to total
+    we could have a total variable of value 0,
+    then loop through array adding each element to total
 
 
 """
@@ -49,32 +46,28 @@ def sumArray(arr):
     Returns the sum of elements in an array
     """
 
-    tot = 0.0
+    tot = 0
 
-    if checkInput(arr) == 0:
-        # Check inf input is an array
-        return ("Please supply a valid array.")
-
-    if len(arr) == 0:
-        # check if array is empty
-        return 0
 
     for el in arr:
-        if type(el) == int or type(el) == float:
-            tot += el
-
-        else:
-            return ("The array elements are not integers.")
-
-    return (f"The sum of the array is {tot}")
+        tot += el
+    
+    tot = round(tot, 4)
+    return (tot)
+    #return sum(arr)
 
 
+arr = []
 if len(sys.argv) < 2:
-    print("No arguments provided")
+    # In case of empty array i.e no elements given, write this
+    print("please input an array in list form, with items separated in spaces")
 else:
-    arr = []
-    for i in range(1, len(sys.argv)):
-        arr.append(i)
+    
+    try:
+        for i in range(1, len(sys.argv)):
+            arr.append(float(sys.argv[i]))
+        print(f"{sumArray(arr)}")
 
-
-    print(sumArray(arr))
+    except ValueError:
+        # In the case where elements are not numbers
+        print("please input numbers in list form, separated with spaces")
